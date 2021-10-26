00f0edb81526de361061b505d76300b5fa968829
AlikhanZhomartov <alikhansoftwareengineer@gmail.com>
2021-10-26 15:38:39 +0600

    Authorization

--- src/main/java/com/example/teamalfa/TeamAlfaApplication.java
+++ src/main/java/com/example/teamalfa/TeamAlfaApplication.java
@@ -4,11 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TeamAlfaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeamAlfaApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 }
