c13d1411bc3f9e9d8e460314a28232a2296f4e8c
AlikhanZhomartov <alikhansoftwareengineer@gmail.com>
2021-10-27 04:16:57 +0600

    Authorization

--- src/main/java/com/example/teamalfa/service/document/DocumentService.java
+++ src/main/java/com/example/teamalfa/service/document/DocumentService.java
@@ -4,13 +4,14 @@ import com.example.teamalfa.exception.ExceptionDescription;
 import com.example.teamalfa.exception.domain.RepositoryException;
 import com.example.teamalfa.model.document.Document;
 import com.example.teamalfa.repository.document.DocumentRepository;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Base64;
 
 @Service
 public class DocumentService {
@@ -22,11 +23,10 @@ public class DocumentService {
         this.documentRepository = documentRepository;
     }
 
-    public Document createDocument(String username, String documentBase64, String fileName) {
+    public Document createDocument(String documentBase64, String fileName) {
         Document document = new Document();
         document.setPdfBase64(documentBase64);
         document.setFileName(fileName);
-        saveInFolder(username, fileName, documentBase64);
         try {
             return documentRepository.save(document);
         } catch (Exception e) {
@@ -45,19 +45,29 @@ public class DocumentService {
         }
     }
 
-    String saveInFolder(String receiver, String fileName, String documentBase64) {
-        File file = new File("C:" + File.separator + "Documents" + File.separator + receiver + File.separator + fileName);
-        if (file.mkdir()) {
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                byte[] decoder = Base64.getDecoder().decode(documentBase64);
-                fos.write(decoder);
-                return file.getPath();
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new RepositoryException(String.format(ExceptionDescription.RepositoryException, "document", "creating"));
-            }
+    public String signDocument(String documentBase64, String fileName, String singImageBase64) {
+        com.itextpdf.text.Document document = new com.itextpdf.text.Document();
+        com.itextpdf.text.Document document1 = new com.itextpdf.text.Document();
+        String documentFolder = "C:" + File.separator + "Documents" + File.separator + fileName + ".pdf";
+        String singImageFolder = "C:" + File.separator + "Documents" + File.separator + fileName + "signImage" + ".pdf";
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(documentFolder));
+            PdfWriter.getInstance(document1, new FileOutputStream(singImageFolder));
+
+            document.open();
+            document1.open();
+            byte[] decoded = Base64.decodeBase64(documentBase64.getBytes());
+            byte[] decoded1 = Base64.decodeBase64(singImageBase64.getBytes());
+            document.add(Image.getInstance(decoded));
+            document1.add(Image.getInstance(decoded1));
+            document.close();
+            document1.close();
         }
-        throw new RepositoryException(String.format(ExceptionDescription.RepositoryException, "document", "creating"));
+        catch (Exception e) {
+            e.printStackTrace();
+            throw new RepositoryException(String.format(ExceptionDescription.RepositoryException, "signed document", "creating"));
+        }
+        return "";
     }
 
 }
